35a1f91dc18c89e9bfcdeffa07a335d0a5f29041
Cardoso <Joo Cardoso>
2026-02-02 14:03:55 +0000

    ...

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/rafael/Projeto-ic/venv
+set -gx VIRTUAL_ENV /home/cardoso/Desktop/Projeto-ic/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
